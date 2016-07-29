a875d66c68045fa396064e9a9fe9335008ab21b2
Charles Fulton <fultonc@lafayette.edu>
2016-07-29 10:35:59 -0400

    Feedback Viewer local plugin v1.2.1

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version   = 2016072900;
+$plugin->version   = 2016072901;
 $plugin->requires  = 2016052300;
 $plugin->component = 'local_feedbackviewer';
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release   = 'v1.2.0';
+$plugin->release   = 'v1.2.1';
